6d3d13b2ef0302b301d6427131f5c598a706e01c
Julien BONACHERA <jbonachera@xebia.fr>
2017-04-04 23:47:43 +0200

    do not force settings file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chown nodered /var/lib/nodered
-exec su nodered -c "node-red -u /var/lib/nodered -s /var/lib/nodered/settings.js $@"
+exec su nodered -c "node-red -u /var/lib/nodered $@"
